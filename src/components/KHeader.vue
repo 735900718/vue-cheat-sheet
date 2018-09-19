3402418cb2d35611f0b00c91bc24a06b8dc4232b
梁玉珂 <liangyk@mapbar.com>
2018-09-19 11:40:54 +0800

    抽出页面模型，添加分页，添加vue-router

---
+++ src/components/KHeader.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-tabs v-model="active" type="card" @tab-click="handleClick">
+    <el-tab-pane
+      v-for="(item, i) in tabs"
+      :key="i"
+      :label="item"
+      :name="item">{{ item }}</el-tab-pane>
+  </el-tabs>
+</template>
+
+<script>
+export default {
+  name: 'k-header',
+  props: {
+    currentTab: String,
+    tabs: Array
+  },
+  data () {
+    return {
+      active: this.currentTab
+    }
+  },
+  methods: {
+    handleClick: function() {
+      this.$emit('update:currentTab', this.active)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/config.scss';
+.el-tabs__header {
+  padding: 10px 10px 0;
+}
+.el-tabs__content {
+  display: none;
+}
+.el-tabs__item {
+  color: #fff;
+  opacity: 0.8;
+  background: $c0;
+  &:hover {
+    color: #fff;
+  }
+  &.is-active {
+    color: #fff;
+    opacity: 1;
+  }
+}
+.el-tabs--card>.el-tabs__header .el-tabs__item.is-active,
+.el-tabs--card>.el-tabs__header,
+.el-tabs--card>.el-tabs__header .el-tabs__nav{
+  border-color: $c0;
+}
+
+</style>
---
+++ src/data/vueRouterMenu.js
@@ -0,0 +1,79 @@
+export default {
+  "Props": {
+    "title": "router-link",
+    "url": "#router-link-props",
+    "sub": {
+      "to": "#to",
+      "replace": "#replace",
+      "append": "#append",
+      "tag": "#tag",
+      "active-class": "#active-class",
+      "exact": "#exact",
+      "event": "#event",
+      "exact-active-class": "#exact-active-class"
+    }
+  },
+  "Props ": {
+    "title": "router-view",
+    "url": "#router-view-props",
+    "sub": {
+      "name": "#name"
+    }
+  },
+  "构建选项": {
+    "title": "Options",
+    "url": "#router-构建选项",
+    "sub": {
+      "routes": "#routes",
+      "mode": "#mode",
+      "base": "#base",
+      "linkActiveClass": "#linkactiveclass",
+      "linkExactActiveClass": "#linkexactactiveclass",
+      "scrollBehavior": "#scrollbehavior",
+      "parseQuery / stringifyQuery": "#parsequery-stringifyquery",
+      "fallback": "#fallback"
+    }
+  },
+  "实例属性": {
+    "title": "Instance",
+    "url": "#router-实例属性",
+    "sub": {
+      "router.app": "#router-app",
+      "router.mode": "#router-mode",
+      "router.currentRoute": "#router-currentroute"
+    }
+  },
+  "实例方法": {
+    "title": "Instance",
+    "url": "#router-实例方法",
+    "sub": {
+      "router.beforeEach": "#router-beforeeach",
+      "router.beforeResolve": "#router-beforeresolve",
+      "router.afterEach": "#router-aftereach",
+      "router.push": "#router-push",
+      "router.replace": "#router-replace",
+      "router.go": "#router-go",
+      "router.back": "#router-back",
+      "router.forward": "#router-forward",
+      "router.getMatchedComponents": "#router-getmatchedcomponents",
+      "router.resolve": "#router-resolve",
+      "router.addRoutes": "#router-addroutes",
+      "router.onReady": "#router-onready",
+      "router.onError": "#router-onerror"
+    }
+  },
+  "属性": {
+    "title": "路由对象",
+    "url": "#路由对象",
+    "sub": {
+      "$route.path": "#路由对象",
+      "$route.params": "#路由对象",
+      "$route.query": "#路由对象",
+      "$route.hash": "#路由对象",
+      "$route.fullPath": "#路由对象",
+      "$route.matched": "#路由对象",
+      "$route.name": "#路由对象",
+      "$route.redirectedFrom": "#路由对象"
+    }
+  }
+}
