378e5be7f2f73deeea9ff71ff5820fa682c9dde3
梁玉珂 <liangyk@mapbar.com>
2018-09-19 11:41:14 +0800

    优化打包

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import elementUI from 'element-ui'
+import {
+  Tabs,
+  TabPane
+} from 'element-ui'
+
 import './assets/reset.css'
+
+Vue.component(Tabs.name, Tabs);
+Vue.component(TabPane.name, TabPane);
+
+import 'element-ui/lib/theme-chalk/index.css';
 
 new Vue({
   el: '#app',
